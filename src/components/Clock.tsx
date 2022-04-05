40bcc236f419561bedfa2fc87d6d012130010821
Moises Velasquez <mvelasquez@pictoric.com>
2022-04-04 23:55:17 -0500

    FIX componentDidUnmount in components using setInterval

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -2,28 +2,17 @@ import React, { useState, useEffect } from "react";
 import Watch from "./Watch";
 
 const Clock = () => {
-	const [hour, setHour] = useState(new Date().toLocaleTimeString());
 	const [visible, setVisible] = useState(false);
 
 	useEffect(() => {
-		let timer: any;
-		if (visible) {
-			timer = setInterval(() => {
-				setHour(new Date().toLocaleTimeString());
-			}, 1000);
-		} else {
-			clearInterval(timer);
-		}
-		return () => {
-			clearInterval(timer);
-		};
-	}, [visible]);
+		console.log("This component mounted");
+	}, []);
 
 	return (
 		<div>
 			{visible && (
 				<h3>
-					Time is: <Watch hour={hour} />
+					Time is: <Watch visible={visible} />
 				</h3>
 			)}
 			<div>
--- src/components/Watch.tsx
+++ src/components/Watch.tsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 type Props = {
-	hour: string;
+	visible: boolean;
 };
 
-const Watch = ({ hour }: Props) => {
+const Watch = ({ visible }: Props) => {
+	const [hour, setHour] = useState(new Date().toLocaleTimeString());
+
 	useEffect(() => {
+		let timer: any;
+		if (visible) {
+			timer = setInterval(() => {
+				setHour(new Date().toLocaleTimeString());
+			}, 1000);
+		} else {
+			clearInterval(timer);
+		}
 		return () => {
-			console.log("this component is didmount");
+			clearInterval(timer);
+			console.log("This component didmount");
 		};
-	}, [hour]);
+	}, [visible]);
+
 	return <span>{hour}</span>;
 };
 
