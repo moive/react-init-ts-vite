9a553125760dcd5eab2526e7f31eb8a3e98f3f34
Moises Velasquez <mvelasquez@pictoric.com>
2022-04-07 17:07:59 -0500

    fetch pokemons

--- src/Views/Pokemons.tsx
+++ src/Views/Pokemons.tsx
@@ -1,18 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Pokemon from "../components/Pokemons/Pokemon";
 
+interface Pokemon {
+	id: string;
+	name: string;
+	avatar: string;
+	url?: string;
+	types: [];
+}
+
 const Pokemons = () => {
-	const [pokemons, setPokemons] = useState([]);
+	const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+	useEffect(() => {
+		setPokemons([]);
+		getAllPokemons("https://pokeapi.co/api/v2/pokemon/");
+		console.log(123);
+	}, []);
+
+	const getAllPokemons = async (url: any) => {
+		const res = await fetch(url);
+		const data = await res.json();
+		// console.log(data.results);
+		data.results.forEach(async (p: Required<Pokemon>) => {
+			const res = await fetch(p.url);
+			const data = await res.json();
+			let pokemon = {
+				id: data.id,
+				name: data.name,
+				avatar: data.sprites.other.dream_world.front_default,
+				types: data.types,
+				// url: "",
+			};
+			console.log(data);
+			setPokemons((pokemons) => [...pokemons, pokemon]);
+		});
+	};
+
 	return (
 		<>
-			<h2>Asynchronous requests</h2>
-			{pokemons.length === 0 ? (
-				<h3>Loading...</h3>
-			) : (
-				pokemons.map((pok) => (
-					<Pokemon key={pok.id} name={pok.name} avatar={pok.avatar} />
-				))
-			)}
+			<h2 className="text-center text-3xl font-bold my-10 uppercase">
+				Asynchronous requests {pokemons.length}
+			</h2>
+			<section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-5 mt-5">
+				{pokemons.length === 0 ? (
+					<h3>Loading...</h3>
+				) : (
+					pokemons.length > 0 &&
+					pokemons.map((pok: Pokemon) => (
+						<Pokemon
+							key={pok.id}
+							name={pok.name}
+							avatar={pok.avatar}
+							types={pok.types}
+						/>
+					))
+				)}
+			</section>
 		</>
 	);
 };
--- src/components/Pokemons/Pokemon.tsx
+++ src/components/Pokemons/Pokemon.tsx
@@ -1,15 +1,26 @@
 import React, { FC } from "react";
 
+type arrPoke = {
+	type: {
+		name: string;
+	};
+};
+
 type Props = {
 	avatar: string;
 	name: string;
+	types: Array<arrPoke>;
 };
 
-const Pokemon: FC<Props> = ({ avatar, name }) => {
+const Pokemon: FC<Props> = ({ avatar, name, types }) => {
 	return (
-		<figure>
+		<figure
+			className={`flex justify-center shadow-md rounded-md py-3 bg-white flex-col justify-between ${types[0].type.name}`}
+		>
 			<img src={avatar} alt={name} />
-			<figcaption>{name}</figcaption>
+			<figcaption className="text-xl font-bold capitalize text-center">
+				{name}
+			</figcaption>
 		</figure>
 	);
 };
