5966e28c31689a2ed5073f786c28ee35afbfaa85
Moises Velasquez <mvelasquez@pictoric.com>
2022-05-01 23:46:53 -0500

    add logic to modal component

--- src/Views/Modals.tsx
+++ src/Views/Modals.tsx
@@ -4,22 +4,48 @@ import { useModal } from "../hooks/useModal";
 
 const Modals = () => {
 	const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+	const [isOpenModal2, openModal2, closeModal2] = useModal(false);
 	return (
-		<div>
-			<Modal>
+		<div className="mt-10">
+			<Modal
+				isOpen={isOpenModal1 as boolean}
+				closeModal={closeModal1 as () => void}
+			>
 				<h3>Modal 1</h3>
 				<p>Hi, that is the content modal 1</p>
-				<img src="https://placeimg.com/400/400/animals" alt="animals" />
+				<img
+					src="https://placeimg.com/400/200/animals"
+					alt="animals"
+					className="w-full"
+				/>
+			</Modal>
+			<Modal
+				isOpen={isOpenModal2 as boolean}
+				closeModal={closeModal2 as () => void}
+			>
+				<h3>Modal 2</h3>
+				<p>Other modal</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Harum odio quis est ea neque possimus temporibus aspernatur
+					id placeat! Adipisci quod officia incidunt similique eius?
+					Error maxime dolores commodi hic!
+				</p>
 			</Modal>
 			<button
+				onClick={openModal1}
+				type="button"
+				className="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mr-3"
+			>
+				Modal 1
+			</button>
+			<button
+				onClick={openModal2}
 				type="button"
 				className="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-				data-bs-toggle="modal"
-				data-bs-target="#exampleModal"
 			>
-				Launch demo modal
+				Modal 2
 			</button>
-			{false && <div className="modal-backdrop fade show"></div>}
 		</div>
 	);
 };
--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,10 +1,11 @@
 import { useState } from "react";
 
-export const useModal = (initilValue = false) => {
-	const [isOpen, setIsOpen] = useState(initilValue);
+export const useModal = (initialValue = false) => {
+	const [isOpen, setIsOpen] = useState<boolean>(initialValue);
 
 	const openModal = () => setIsOpen(true);
+
 	const closeModal = () => setIsOpen(false);
 
-	return [isOpen, openModal, closeModal];
+	return [isOpen, openModal, closeModal] as const;
 };
