650ff4ebebd5b7bc36b911428a3616d32943ecc6
Moises Velasquez <mvelasquez@pictoric.com>
2022-05-12 11:03:32 -0500

    init shopping-cart

---
+++ src/reducers/shoppingReducer.ts
@@ -0,0 +1,40 @@
+import { TYPES } from "../actions/shoppingActions";
+
+interface IState {
+	// count: number;
+}
+
+type Action = {
+	type: string;
+	payload?: number;
+	isInit?: boolean;
+};
+
+export const shoppingInitialState = {
+	products: [
+		{ id: 1, name: "Product 1", price: 100 },
+		{ id: 2, name: "Product 2", price: 200 },
+		{ id: 3, name: "Product 3", price: 300 },
+		{ id: 4, name: "Product 4", price: 400 },
+		{ id: 5, name: "Product 5", price: 500 },
+		{ id: 6, name: "Product 6", price: 600 },
+		{ id: 7, name: "Product 7", price: 700 },
+	],
+	cart: [],
+};
+
+export function shoppingReducer(state: IState, action: Action) {
+	switch (action.type) {
+		case TYPES.ADD_TO_CART: {
+		}
+		case TYPES.REMOVE_ONE_FROM_CART: {
+		}
+		case TYPES.REMOVE_ALL_FROM_CART: {
+		}
+		case TYPES.CLEAR_CART: {
+		}
+
+		default:
+			return state;
+	}
+}
